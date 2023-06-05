6363b29b96f5e880502833ae75863a1372b160cb
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-06-05 18:24:10 +0000

    Deploying to gh-pages from @ Soyniko1076/goit-js-hw-11@eb38802b039cdd473cea4d2190c2b72f256012e3 🚀

---
+++ index.faa55a00.js
@@ -0,0 +1,2 @@
+!function(t){const a=new URLSearchParams({key:"37053525-954bf5b1abb6340838a01bbc5",q:t});fetch(`https://pixabay.com/api/?${a}`).then((t=>{if(!t.ok)throw new Error(t.status);return t.json()})).catch((t=>console.log(t)))}("cat");
+//# sourceMappingURL=index.faa55a00.js.map
