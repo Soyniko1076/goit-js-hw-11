175e5b459b4a4e67c7ce049926a22cf09876fce9
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-06-06 17:04:45 +0000

    Deploying to gh-pages from @ Soyniko1076/goit-js-hw-11@a469c098850cea942f6e957fa41e20d33b9e4537 🚀

--- index.0f14c4fb.js
+++
@@ -1,2 +0,0 @@
-!function(o){const t=new URLSearchParams({key:"37053525-954bf5b1abb6340838a01bbc5",q:o,image_type:photo,orientation:horizontal,safesearch:!0});fetch(`https://pixabay.com/api/?${t}`).then((o=>{if(!o.ok)throw new Error(o.status);return o.json()})).catch((o=>console.log(o)))}("dog");
-//# sourceMappingURL=index.0f14c4fb.js.map
--- index.f9a246cc.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;o=new URLSearchParams({key:"37053525-954bf5b1abb6340838a01bbc5",q:"dog",image_type:photo,orientation:horizontal,safesearch:!0}),fetch("".concat("https://pixabay.com/api/","?").concat(o)).then((function(o){if(!o.ok)throw new Error(o.status);return o.json()})).catch((function(o){return console.log(o)}))}();
-//# sourceMappingURL=index.f9a246cc.js.map
