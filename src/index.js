b016e092701a2101234110e63580c194d7860b3c
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-06-09 00:52:19 +0300

    simple(-)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import { fetchEvents } from './object-api.js';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '37053525-954bf5b1abb6340838a01bbc5';
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   gallery: document.querySelector('.gallery'),
@@ -7,25 +11,75 @@ const refs = {
   loader: document.querySelector('.loader'),
   form: document.querySelector('.search-form'),
   button: document.querySelector('.load-more'),
-  list:document.querySelector('.info')
 };
+
+const lightbox = new SimpleLightbox('.gallery a').refresh();
 
 let pageToFetch = 1;
 let queryToFetch = '';
 
+function fetchEvents(keyword) {
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${keyword}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
 function getEvents(query, page) {
   fetchEvents(query, page)
     .then(data => {
-      Notify.success('Hooray! We found 500 images');
-      const events = data.hits;
-      console.log(events);
-      renderEvents(events);
-      refs.button.classList.remove('unvisible');
+      console.log(data);
+      if (data.totalHits !== 0) {
+        Notify.success('Hooray! We found 500 images');
+        const events = data.hits;
+        console.log(events);
+        renderEvents(events);
+
+        refs.button.classList.remove('unvisible');
+      } else {
+        Notify.failure(
+          `Sorry, there are no images matching your search query "${query}". Please try again.`
+        );
+        refs.button.classList.add('unvisible');
+      }
     })
     .catch(error => {
       console.log(error);
       Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
+        `Sorry, there are no images matching your search query. Please try again later`
+      );
+    });
+}
+
+function loadEvents(query, page) {
+  fetchEvents(query, page)
+    .then(data => {
+      console.log(data);
+      if (data.totalHits !== 0) {
+        const events = data.hits;
+        console.log(events);
+        renderEvents(events);
+        // lightbox.refresh()
+        refs.button.classList.remove('unvisible');
+      } else {
+        Notify.failure(
+          `Sorry, there are no images matching your search query "${query}". Please try again.`
+        );
+        refs.button.classList.add('unvisible');
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again later`
       );
     });
 }
@@ -42,20 +96,35 @@ function renderEvents(events) {
         comments,
         downloads,
       }) => {
-        return `<div class="photo-card  shadow6"><img class="img" src="${webformatURL}" alt="${tags}" loading="lazy" width="330" height="230"><ul class="info"><li class="item"><p class="info-item">Likes</p><p class="descrip">${likes}</p></li><li class="item"><p class="info-item">Views</p><p class="descrip">${views}</p></li><li class="item"><p class="info-item">Comments</p><p class="descrip">${comments}</p></li><li class="item"><p class="info-item">Downloads</p><p class="descrip">${downloads}</p></li></ul></div>`;
+        return `<div class="photo-card  shadow6"><a href="${largeImageURL}"><img class="img" src="${webformatURL}" alt="${tags}" loading="lazy" width="330" height="230"></a><ul class="info"><li class="item"><p class="info-item">Likes</p><p class="descrip">${likes}</p></li><li class="item"><p class="info-item">Views</p><p class="descrip">${views}</p></li><li class="item"><p class="info-item">Comments</p><p class="descrip">${comments}</p></li><li class="item"><p class="info-item">Downloads</p><p class="descrip">${downloads}</p></li></ul></div>`;
       }
     )
     .join('');
+
   refs.gallery.insertAdjacentHTML('beforeend', marcup);
+
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
 
 refs.form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  queryToFetch = event.target.elements.searchQuery.value;
+  const inputValue = event.target.elements.searchQuery.value;
+  if (!inputValue.trim() || inputValue === queryToFetch) {
+    return;
+  }
+  queryToFetch = inputValue;
   pageToFetch = 1;
   refs.gallery.innerHTML = '';
+  refs.button.classList.add('unvisible');
   getEvents(queryToFetch, pageToFetch);
 }
 
@@ -63,5 +132,5 @@ refs.button.addEventListener('click', hendlLoadMore);
 
 function hendlLoadMore() {
   pageToFetch += 1;
-  getEvents(queryToFetch, pageToFetch);
+  loadEvents(queryToFetch, pageToFetch);
 }
